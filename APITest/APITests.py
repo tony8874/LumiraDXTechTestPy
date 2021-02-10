f9ba412ff485ee03a6b935c2b3c3d9475de5a67e
Anthony Crichton <tony.crichton67@gmail.com>
2021-02-10 23:14:03 +0000

    Fix ddt.unpack issue

--- APITest/APITests.py
+++ APITest/APITests.py
@@ -11,8 +11,9 @@ from HelperClass.Endpoints import Endpoint
 class APITestCases(unittest.TestCase):
     def test_CategoryList(self):
         response = RESTClient.GetRequest(URL.BaseURL, Endpoint.BlogCategory)
+        # Status Code verification
         self.assertEqual(200, response.status_code)
-
+        # Content-Type verification
         self.assertEqual('application/json', response.headers['content-type'])
 
         RESTClient.ValidateJson(response.json(), Schema.ListCategorySchemas)
@@ -20,18 +21,18 @@ class APITestCases(unittest.TestCase):
     @ddt.data(('Engineering'), ('Cookery'), ('Science'))
     def test_AddCategory(self, category):
         response = RESTClient.PostRequest(URL.BaseURL, Endpoint.BlogCategory, {"name": ""+category+""})
-
+        # Status Code verification
         self.assertEqual(201, response.status_code)
-
+        # Content-Type verification
         self.assertEqual('application/json', response.headers['content-type'])
 
     @ddt.data(('4', 204), ('5', 204), ('6', 204), ('20', 404))
     @ddt.unpack
     def test_DeleteCategory(self, category_id, status_code):
         response = RESTClient.DeleteRequest(URL.BaseURL, Endpoint.BlogCategory, category_id)
-
+        # Status Code verification
         self.assertEqual(status_code, response.status_code)
-
+        # Content-Type verification
         self.assertEqual('application/json', response.headers['content-type'])
 
 
