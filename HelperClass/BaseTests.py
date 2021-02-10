f587917b33bc429904c48bce4599a3f9275c98ce
Anthony Crichton <tony.crichton67@gmail.com>
2021-02-10 22:46:34 +0000

    ddt update

--- HelperClass/BaseTests.py
+++
@@ -1,8 +0,0 @@
-
-
-class BasicTests:
-    def VerifyBaseTests(self, expected, response):
-        self.assertEqual(expected, response.status_code)
-
-        self.assertEqual(expected, response.headers["content-type"])
-
